a2c8099843678d5f08c1317c21b3090766dd3206
Amit Kumar <amitkumar@Amits-MacBook-Air.local>
2019-01-31 18:48:50 +0530

    adding header component

--- App.jsx
+++ App.jsx
@@ -1,10 +1,51 @@
 import React, { Component } from 'react';
 
+class Header extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-inverse">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <a className="navbar-brand" href="#">
+              React Tutorials
+            </a>
+          </div>
+          <ul className="nav navbar-nav">
+            <li className="active">
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About us</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#">
+                <span className="glyphicon glyphicon-user" /> Sign Up
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <span className="glyphicon glyphicon-log-in" /> Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
 class App extends React.Component {
   render() {
     return (
       <div>
-        Hello Welcome to react<div>First</div>
+        <Header />
+        <div className="jumbotron">
+          Hello Welcome to react<div>First</div>
+        </div>
       </div>
     );
   }
